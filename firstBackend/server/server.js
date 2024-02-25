53685e74ad64bb3a1bd7a33bfb80fc8916580d8a
codespace555 <namansanjaykumar@gmail.com>
2024-02-25 19:03:18 +0530

    add som update

--- firstBackend/server/server.js
+++ firstBackend/server/server.js
@@ -2,7 +2,9 @@ import express from "express";
 import 'dotenv/config'
 const app = express();
 
-
+app.get("/", (req, res) => {
+res.send("Hello Naman")
+})
 app.get("/jokes", (req, res) => {
   const jokes = [
     {
